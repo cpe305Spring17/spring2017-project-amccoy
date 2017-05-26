653847c4aa7f895af0a479992e47d2ec5ee65d35
Aidan McCoy <aidanmccoy2@gmail.com>
2017-05-26 04:53:33 -0700

    Initial Commit

---
+++ app/src/main/java/aidanmccoy/alarmclock/SetAlarmsActivity.java
@@ -0,0 +1,51 @@
+package aidanmccoy.alarmclock;
+
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class SetAlarmsActivity extends AppCompatActivity {
+
+    Button monBtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.set_alarms);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Log.d("tag", "onCreate entered");
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void goToCalendar(View view) {
+        Intent intent = new Intent(this, Calendar.class);
+        startActivity(intent);
+
+    }
+
+    public void onMonBtn(View v) {
+        Log.d("tag", "onMonBtn entered");
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getFragmentManager(), "TimePicker");
+    }
+
+}
