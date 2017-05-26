4a3d11830a64d2913d5c9a5b04d80ec9cb5d2605
Aidan McCoy <aidanmccoy2@gmail.com>
2017-05-26 06:22:01 -0700

    Added sonar properties

--- app/src/main/java/aidanmccoy/alarmclock/TimePickerFragment.java
+++ app/src/main/java/aidanmccoy/alarmclock/TimePickerFragment.java
@@ -6,6 +6,7 @@ import android.app.TimePickerDialog;
 import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
@@ -39,6 +40,8 @@ public class TimePickerFragment extends DialogFragment implements TimePickerDial
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
         pickedTime.setHours(hourOfDay);
         pickedTime.setMinutes(minute);
+        Log.d("tag", "The time set is:" + pickedTime.toString());
+
         TextView monTime = (TextView) getActivity().findViewById(R.id.monTime);
         monTime.setText(String.valueOf(hourOfDay) + ":" + String.valueOf(minute));
     }
